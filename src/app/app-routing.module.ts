083c3e83f1ca0efcc1ea2c1365937b709e86f348
Hirageppa <devraj.hirageppa@capgemini.com>
2025-02-19 17:19:52 +0530

    aaded routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgForComponent } from './component/ng-for/ng-for.component';
+import { UserComponent } from './component/user.component';
+import { DevComponent } from './dev/dev.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "table", component:NgForComponent, pathMatch:'full'
+  },
+  {
+    path: "login", component:UserComponent, pathMatch:'full'
+  },
+  {
+    path: "dev-blocks", component:DevComponent, pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
